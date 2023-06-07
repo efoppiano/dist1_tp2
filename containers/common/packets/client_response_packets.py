c0c2505f837c9b02331d7700d93803e6af384ce8
Nicolas Zulaica <nzulaica@fi.uba.ar>
2023-06-07 15:23:01 -0300

    remove unused

--- containers/common/packets/client_response_packets.py
+++ containers/common/packets/client_response_packets.py
@@ -9,21 +9,6 @@ from common.packets.station_dist_mean import StationDistMean
 from common.packets.trips_count_by_year_joined import TripsCountByYearJoined
 
 
-
-@dataclass
-class DurAvgOutOrEof(BasicPacket):
-    data: Union[DurAvgOut, List[DurAvgOut], Eof]
-
-
-@dataclass
-class TripsCountByYearJoinedOrEof(BasicPacket):
-    data: Union[TripsCountByYearJoined, List[TripsCountByYearJoined], Eof]
-
-
-@dataclass
-class StationDistMeanOrEof(BasicPacket):
-    data: Union[StationDistMean, List[StationDistMean], Eof]
-
 @dataclass
 class GenericResponsePacket(BasicPacket):
     replica_id: int
