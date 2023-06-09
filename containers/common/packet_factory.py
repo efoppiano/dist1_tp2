b5768f80b9451b66ed4c964582015d418c940511
Nicolas Zulaica <nzulaica@fi.uba.ar>
2023-06-09 13:25:36 -0300

    generate packet_id at client

--- containers/common/packet_factory.py
+++ containers/common/packet_factory.py
@@ -7,12 +7,19 @@ DIST_MEAN_REQUEST = b'dist_mean'
 TRIP_COUNT_REQUEST = b'trip_count'
 DUR_AVG_REQUEST = b'dur_avg'
 
-
+MAX_PACKET_ID = None # 2**10 - 1
+def increment_packet_id(func):
+    def wrapper(*args, **kwargs):
+        PacketFactory.packet_id += 1
+        if MAX_PACKET_ID and PacketFactory.packet_id > MAX_PACKET_ID:
+            PacketFactory.packet_id = 0
+        return func(*args, **kwargs)
+    return wrapper
 
 class PacketFactory:
     client_id = None
     replica_id = None
-    packet_id = None
+    packet_id = 0
 
     @staticmethod
     def set_ids( client_id: str ):
@@ -20,16 +27,18 @@ class PacketFactory:
         PacketFactory.replica_id = 0
 
     @staticmethod
+    @increment_packet_id
     def build_id_request_packet(client_id: str) -> bytes:
         return GenericPacket(
             replica_id= None,
             client_id= None,
             city_name= None,
-            packet_id= None,
+            packet_id= PacketFactory.packet_id,
             data=ClientGatewayPacket(ClientIdPacket(client_id)).encode()
         ).encode()
 
     @staticmethod
+    @increment_packet_id
     def build_weather_packet(city_name: str, weather_info: List[WeatherInfo]) -> bytes:
         return GenericPacket(
             replica_id= PacketFactory.replica_id,
@@ -40,6 +49,7 @@ class PacketFactory:
         ).encode()
 
     @staticmethod
+    @increment_packet_id
     def build_weather_eof(city: str) -> bytes:
         return GenericPacket(
             replica_id= PacketFactory.replica_id,
@@ -52,6 +62,7 @@ class PacketFactory:
         ).encode()
 
     @staticmethod
+    @increment_packet_id
     def build_station_packet(city_name: str, station_info: List[StationInfo]) -> bytes:
         return GenericPacket(
             replica_id= PacketFactory.replica_id,
@@ -62,6 +73,7 @@ class PacketFactory:
         ).encode()
 
     @staticmethod
+    @increment_packet_id
     def build_station_eof(city: str) -> bytes:
         return GenericPacket(
             replica_id= None,
@@ -75,23 +87,25 @@ class PacketFactory:
 
 
     @staticmethod
+    @increment_packet_id
     def build_trip_packet(city_name: str, trip_info: List[TripInfo]) -> bytes:
         return GenericPacket(
             replica_id= None,
             client_id= PacketFactory.client_id,
             city_name= city_name,
-            packet_id= None,
+            packet_id= PacketFactory.packet_id,
             data=ClientGatewayPacket(trip_info).encode()
         ).encode()
     
 
     @staticmethod
+    @increment_packet_id
     def build_trip_eof(city: str) -> bytes:
         return GenericPacket(
             replica_id= None,
             client_id= PacketFactory.client_id,
             city_name= city,
-            packet_id= None,
+            packet_id= PacketFactory.packet_id,
             data=ClientGatewayPacket(
                 ClientEofPacket("trip", PacketFactory.client_id, city)
             ).encode()
