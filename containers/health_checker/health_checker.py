fa3e1f546dd7464ce51a2133578e17f73bc59e5a
Nicolas Zulaica <nzulaica@fi.uba.ar>
2023-06-14 13:54:58 -0300

    remove is_alive

--- containers/health_checker/health_checker.py
+++ containers/health_checker/health_checker.py
@@ -8,35 +8,12 @@ from common.basic_health_checker import BasicHealthChecker
 
 CONTAINERS = os.environ["CONTAINERS"]
 
-
-# ! FIXME: REMOVE THIS - ONLY FOR DEBUGGING
-def is_alive(container_name):
-    """Checks if a container with the given name is alive by its status"""
-    container_name = f"tp2-{container_name}-1"
-    result = subprocess.run(
-        ["docker", "inspect", container_name, "--format='{{.State.Status}}'"],
-        stdout=subprocess.PIPE, check=False
-    )
-    if result.returncode != 0:
-        return False
-
-    status = result.stdout.decode("utf-8").strip()
-    logging.info("Container %s is %s", container_name, status)
-
-    if status == "'exited'":
-        return False
-
-    return True
-
-
 class HealthChecker(BasicHealthChecker):
     def on_check_fail(self, container_name: str):
-        if is_alive(container_name):
-            logging.critical("Container %s is already alive", container_name)
 
         container_name = f"tp2-{container_name}-1"
         start = time.time()
-        result = subprocess.run(
+        subprocess.run(
             ["docker", "start", container_name],
             stdout=subprocess.PIPE, check=False
         )
