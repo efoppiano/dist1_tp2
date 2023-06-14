4eb150a16f7e115c41ea08357fcc973fc88179c1
Nicolas Zulaica <nzulaica@fi.uba.ar>
2023-06-13 22:04:11 -0300

    add healthcheck envvars

--- scripts/build.py
+++ scripts/build.py
@@ -14,8 +14,8 @@ def increase_prev_amount(container_name, amount):
   data["containers"][container_name]["prev_amount"] += amount
 
 
-for name, container in data["containers"].items():
-  next = container["next"]
+for name, container_data in data["containers"].items():
+  next = container_data["next"]
 
   if isinstance(next, str):
     if next not in data["containers"]:
@@ -25,15 +25,35 @@ for name, container in data["containers"].items():
     if name == "gateway":
       increase_prev_amount(next, 1)
     else:
-      increase_prev_amount(next, container["amount"])
+      increase_prev_amount(next, container_data["amount"])
   else:
     next_amount = {}
     for container_name in next:
       next_amount[container_name] = data["containers"][container_name]["amount"]
-      increase_prev_amount(container_name, container["amount"])
+      increase_prev_amount(container_name, container_data["amount"])
 
-  container["next_amount"] = next_amount
+  container_data["next_amount"] = next_amount
 
+
+# Health Check
+_health_check_containers = []
+for name, container_name in data["containers"].items():
+  for i in range(container_name["amount"]):
+    _health_check_containers.append(f"{name}_{i}")
+_health_check_containers.append("response_provider")
+
+HEALTH_CHECKER_AMOUNT = 2
+health_check_containers = []
+containers_health_checkers = {}
+for i in range(HEALTH_CHECKER_AMOUNT):
+  containers = _health_check_containers[i::HEALTH_CHECKER_AMOUNT][:]
+  next_n = (i + 1) % HEALTH_CHECKER_AMOUNT
+
+  for container_name in containers:
+    containers_health_checkers[container_name] = f"health_checker_{i}"
+
+  containers.append(f"health_checker_{next_n}")
+  health_check_containers.append(containers)
 ### ----------------- RABBIT ----------------- ###
 
 
@@ -79,6 +99,8 @@ def add_container(name, container, n):
   env["INPUT_QUEUE"] = f"{name}_{n}"
   env["EOF_ROUTING_KEY"] = name
   env["REPLICA_ID"] = n
+  env["HEALTH_CHECKER"] = containers_health_checkers[f"{name}_{n}"]
+
   if "prev_amount" in container:
     env["PREV_AMOUNT"] = container["prev_amount"]
   else:
@@ -133,6 +155,7 @@ def add_response_provider():
     environment:'''
 
   env = data["common_env"].copy()
+  env["HEALTH_CHECKER"] = containers_health_checkers[name]
 
   for src_type, provider in data["response_provider"].items():
     provider_amount = data["containers"][provider]["amount"]
@@ -152,6 +175,7 @@ add_response_provider()
 def add_health_checker(n, containers):
   global output
   name = "health_checker"
+  next_n = (n + 1) % HEALTH_CHECKER_AMOUNT
 
   output += f'''
 
@@ -170,28 +194,15 @@ def add_health_checker(n, containers):
 
   env = data["common_env"].copy()
   env["CONTAINERS"] = ",".join(containers)
+  env["HEALTH_CHECKER"] = f"health_checker_{next_n}"
 
   for key, value in env.items():
     output += f'''
       - {key}={value}'''
 
 
-def get_all_containers():
-  all_containers = []
-  for name, container in data["containers"].items():
-    for i in range(container["amount"]):
-      all_containers.append(f"{name}_{i}")
-  all_containers.append("response_provider")
-
-  return all_containers
-
-
-all_containers = get_all_containers()
-HEALTH_CHECKER_AMOUNT = 2
 for i in range(HEALTH_CHECKER_AMOUNT):
-  containers = all_containers[i::HEALTH_CHECKER_AMOUNT][:]
-  next_n = (i + 1) % HEALTH_CHECKER_AMOUNT
-  containers.append(f"health_checker_{next_n}")
+  containers = health_check_containers[i]
   add_health_checker(i, containers)
 
 ### ----------------- CLIENTS ----------------- ###
